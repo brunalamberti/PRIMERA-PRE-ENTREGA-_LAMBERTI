dff9eba2520c56e52cd19c88b21e23d011bc9825
brunalamberti <brunalamberti96@gmail.com>
2023-03-12 16:52:25 -0300

    Archivo JS

---
+++ DUDAS/dudas.js
@@ -0,0 +1,81 @@
+alert("Bienvenido a mi tienda online");
+
+const agregarProducto = () =>{
+    let producto = "";
+    let cant = 0;
+    let seguirComprando = false;
+    let carrito = [ ];
+   
+    do {
+producto = prompt("¿Desea comprar fideos, salsa o queso, o un combo de los tres?").toLowerCase();
+
+while ((producto !== "fideos") && (producto !== "salsa") && (producto !== "queso") && (producto !== "combo") &&
+(producto !== "combo de los tres") && (producto !== "los 3") && (producto !== "3")){
+     alert("El producto elegido no se encuentra disponible actualmente.");
+     producto = prompt("¿Desea comprar fideos, salsa o queso, o un combo de los tres?").toLowerCase();
+}
+cant = parseInt(prompt("¿Cuántos quiere llevar?"));
+
+while (Number.isNaN(cant) || cant === 0){
+alert("Debe seleccionar una cantidad válida.")
+cant = parseInt(prompt("¿Cuántos quiere llevar?"));
+    }
+
+for (let i=0; i<= cant; i++){
+carrito.push(producto);
+}
+
+seguirComprando = confirm('¿Desea seguir comprando?');
+    }while (seguirComprando);
+
+
+
+};
+
+
+return carrito;
+// function calcularPrecio(){
+//     let subtotal = 0;
+// for (let item of carrito){
+//     switch(item){
+//         case "fideos":
+//            precioProd= 200;
+//            break;
+//        case "salsa":
+//            precioProd = 280;
+//            break;    
+//        case "queso":
+//            precioProd = 150;    
+//            break;
+//        case "combo", "combo de los tres","los 3", "3":
+//            precioProd = 500;
+//            break;
+//        default:("Alguno de los datos ingresados no es correcto")
+//            precioProd = 0,
+//            cantidad = 0;
+//            break;
+//        }
+// subtotal = subtotal + precioProd;
+// }
+// return subtotal;
+// }
+
+// function calcularPrecio(){
+//     let subtotal = 0;
+// for (let item of carrito){
+//     if (item === "fideos"){
+//         subtotal = subtotal + 200;}
+//         else if (item === "salsa"){
+//             subtotal = subtotal +300;
+//         }else if (item === "queso"){
+//             subtotal = subtotal+150;
+//             }else{
+//                 subtotal = subtotal + 500;
+//             }
+// return subtotal;
+// }
+// }
+
+agregarProducto();
+// calcularPrecio();
+// alert("El total de su compra es  " + subtotal);
--- main.js
+++ main.js
@@ -1,6 +1,52 @@
 alert("Bienvenido a mi tienda online");
 
-function calcularPrecioFinal(){
+const agregarProducto = () =>{
+    let producto = "";
+    let cant = 0;
+    let subtotal = 0;
+    let precioProd =0;
+    let seguirComprando = false;
 
+
+   
+do {
+producto = prompt("¿Desea comprar fideos, salsa o queso, o un combo de los tres?").toLowerCase();
+
+while ((producto !== "fideos") && (producto !== "salsa") && (producto !== "queso") && (producto !== "combo") &&
+(producto !== "combo de los tres") && (producto !== "los 3") && (producto !== "3")){
+     alert("El producto elegido no se encuentra disponible actualmente.");
+     producto = prompt("¿Desea comprar fideos, salsa o queso, o un combo de los tres?").toLowerCase();
 }
-calcularPrecioFinal();
+cant = parseInt(prompt("¿Cuántos quiere llevar?"));
+
+while (Number.isNaN(cant) || cant === 0){
+alert("Debe seleccionar una cantidad válida.")
+cant = parseInt(prompt("¿Cuántos quiere llevar?"));
+    }
+
+switch(producto){
+        case "fideos":
+                   precioProd= 200;
+                   break;
+               case "salsa":
+                   precioProd = 280;
+                   break;    
+               case "queso":
+                   precioProd = 150;    
+                   break;
+               case "combo": case "combo de los tres": case "los 3": case "3":
+                   precioProd = 500;
+                   break;
+               default:("Alguno de los datos ingresados no es correcto")
+                   precioProd = 0,
+                   cantidad = 0;
+                   break;
+               }
+    subtotal += precioProd * cant;
+ seguirComprando = confirm('¿Desea seguir comprando?');
+    } while (seguirComprando);
+    alert("El total de su compra es de $"+subtotal + ". Muchas gracias por su compra.")
+}
+
+agregarProducto();
+;
